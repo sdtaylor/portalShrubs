b1429471f04c743954bdbf96fc21bc5042feb61e
Shawn <shawntaylor@weecology.org>
2015-11-12 10:30:56 -0500

    added percent cover function

--- processRawTransectData.R
+++ processRawTransectData.R
@@ -1,8 +1,28 @@
 library(tidyr)
+library(dplyr)
 csvFolder='~/projects/portalShrubs/shrubData/excelExports/'
 
 #Each year of transect data is slightly different, so I will just handle
 #each seperatly.
+
+
+#Once each year is in the format data.frame(Transect, Point, Plot, Year, Group)
+#pass it to this to get percent cover. 
+processFormattedData=function(df){
+  transectTotals=df %>%
+    group_by(Year, Plot) %>%
+    summarize(totalPoints=n())
+  
+  df=df %>%
+    group_by(Year, Plot, Group) %>%
+    summarize(n=n())
+  
+  df=merge(df, transectTotals, by=c('Year','Plot'))
+  df=df %>% mutate(cover = n/totalPoints)
+    
+  return(select(df, -n, -totalPoints))
+}
+
 
 #################################
 #year 1989. 
@@ -16,8 +36,13 @@ year89$Plot=substring(year89$Plot, 5)
 year89$Year=1989
 
 #Assign species name
-speciesList89=read.csv(paste(csvFolder, '1989_species_list.csv',sep=''))
-year89=merge(year89, speciesList89, by.x='SpeciesID',by.y='Number')
+speciesList89=read.csv(paste(csvFolder, '1989_species_list_ShawnsEdits.csv',sep=''))
+year89=merge(year89, speciesList89, by.x='SpeciesID',by.y='Number') %>%
+  select(Transect, Point, Plot, Year, Group)
+
+
+year89=processFormattedData(year89)
+
 
 ###############################
 #year 1992
@@ -31,7 +56,7 @@ year92$Plot=substring(year92$Plot, 5)
 year92$Year=1992
 
 #Assign species name
-speciesList92=read.csv(paste(csvFolder, '1992_species_list.csv',sep=''))
+speciesList92=read.csv(paste(csvFolder, '1992_species_listShawnsEdits.csv',sep=''))
 year92=merge(year92, speciesList92, by.x='SpeciesID',by.y='Number')
 
 ##############################
